2c14ce99432c74d8d38156ea789dab3d3bb4d508
dwiprst13 <dwiprst13@gmail.com>
2025-09-13 08:06:39 +0700

    Create budget_controller.go

---
+++ controllers/budget_controller.go
@@ -0,0 +1,48 @@
+package controllers
+
+import (
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+    "money-tracker/config"
+    "money-tracker/models"
+    "money-tracker/utils"
+)
+
+func SetBudget(c *gin.Context) {
+    userID := c.GetString("userID")
+
+    var input struct {
+        Category string  `json:"category" binding:"required"`
+        Amount   float64 `json:"amount" binding:"required"`
+    }
+    if err := c.ShouldBindJSON(&input); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    var budget models.Budget
+    err := config.DB.Where("user_id = ? AND category = ?", utils.ParseUint(userID), input.Category).First(&budget).Error
+    if err != nil {
+        // create baru
+        budget = models.Budget{
+            UserID:   utils.ParseUint(userID),
+            Category: input.Category,
+            Amount:   input.Amount,
+        }
+        config.DB.Create(&budget)
+    } else {
+        // update
+        budget.Amount = input.Amount
+        config.DB.Save(&budget)
+    }
+
+    c.JSON(http.StatusOK, budget)
+}
+
+func GetBudgets(c *gin.Context) {
+    userID := c.GetString("userID")
+    var budgets []models.Budget
+    config.DB.Where("user_id = ?", utils.ParseUint(userID)).Find(&budgets)
+    c.JSON(http.StatusOK, budgets)
+}
